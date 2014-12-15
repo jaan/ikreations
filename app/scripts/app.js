71c428ac464a7d5cc4b3f42d3843d460475c2017
jaan <njraju@gmail.com>
2014-12-14 17:01:58 -0800

    updating the project with grutn tasks and processes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,11 @@
-'use strict'
-console.log("hello");
+(function(document) {
+  'use strict';
+
+  document.addEventListener('polymer-ready', function() {
+    // Perform some behaviour
+    console.log('Polymer is ready to rock!');
+  });
+
+// wrap document so it plays nice with other libraries
+// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+})(wrap(document));
